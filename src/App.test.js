bf432116593fce0d8da5768ff613398ecaa2d72b
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2022-03-21 14:58:48 -0600

    renders a Json test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from "./data.json";
 
 describe('starwars app', () => {
   it('should show a list of characters including Luke Skywalker', () => {
     render(<App />);
     expect(screen.getByText('Luke Skywalker')).toBeInTheDocument();
+  });
+  
+  it('Should show a list of characters from a JSON file',()=>{
+    render(<App />);
+    for (let character of data.results) {
+      expect(screen.getByText(character.name)).toBeInTheDocument();
+    }
   });
   
 });
